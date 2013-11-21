16bf5b5fde3ee5a618521a80a4a13ef85e74c0fc
Oliver Valido <alu0100598148@ull.edu.es>
2013-11-21 22:04:10 +0000

    Prueba solucionada implmentando traspuesta y determinante de matriz dispersa

--- lib/matrizDispersa.rb
+++ lib/matrizDispersa.rb
@@ -30,5 +30,42 @@ class MatrizDispersa < Matriz
     end
     matString
   end
+  
+  def tras
+    aux = MatrizDispersa.new(@mCol, @nFil, {})
+    for i in 0...@nFil do
+      for j in 0...@mCol do
+        if ((!@matriz[j].nil?) && (!@matriz[j][i].nil?))
+            if (aux.matriz[i].nil?)
+              aux.matriz[i] = {}
+            end
+          aux.matriz[i][j] = @matriz[j][i]
+        end
+      end
+    end
+    aux
+  end
 
+  def det
+    if (@nFil == @mCol)
+      if ((!@matriz[0].nil?) && (!@matriz[0][0].nil?))
+        det = @matriz[0][0]
+      else
+        det = 0
+      end
+      aux = MatrizDispersa.new(@nFil, @mCol, @matriz)
+      for k in 0...@nFil do
+        l = k+1
+        for i in l...@mCol do
+          for j in l...@mCol do
+            aux.matriz[i][j] = (aux.matriz[k][k] * aux.matriz[i][j] - aux.matriz[k][j] * aux.matriz[i][k])/ aux.matriz[k][k]
+          end
+        end
+        det = det * aux.matriz[k][k]
+      end
+    else
+      det = "ERROR, la matriz no es cuadrada"
+    end
+    det
+  end
 end
